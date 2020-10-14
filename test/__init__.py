94617cb15b7965947525cdaef8c4869c82f7a13d
zz <qs114115@163.com>
2020-10-14 21:19:46 +0800

    zz

---
+++ test/__init__.py
@@ -0,0 +1,14 @@
+import requests
+
+if __name__ == "__main__":
+    #指定url
+    url = 'https://www.sogou.com/'
+    #发起请求
+    #接收返回的响应对象
+    response = requests.get(url=url)
+    #获取响应数据.text返回的是str形式的响应数据
+    page_text = response.text
+    print(page_text)
+    #持久化存储
+    with open('sogou.html','w',encoding='utf-8') as fp:
+        fp.write(page_text)
